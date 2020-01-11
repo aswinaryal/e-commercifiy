d8562b5fad888bab25c1364fa3b37c1f61491945
Ashwin J Chhetri <ashwinjchhetri@MacBook-Pro-di-Ashwin.local>
2020-01-11 12:10:40 +0545

    react router navigation integrated into hats page

--- src/App.js
+++ src/App.js
@@ -1,11 +1,16 @@
 import React from 'react';
 import HomePage from './pages/homepage/homepage.pages';
-
+import {Route,Switch} from 'react-router-dom';
+import HatsPage from './pages/hatspage/hatspage.pages';
 
 function App() {
   return (
     <div>
-      <HomePage />
+      <Switch>
+        <Route exact path='/' component = {HomePage} />
+        <Route path='/hats' component = {HatsPage} />
+      </Switch>
+      
      </div>
   )
 }
